f6926fb6df3f54202054711cc7befce04374ab0c
KenyStev <kjestevez.0795@gmail.com>
2014-11-26 15:52:47 -0600

    class Global e informacion

---
+++ src/tareavisual2/Global.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareavisual2;
+
+/**
+ *
+ * @author zerokull
+ */
+public class Global {
+    public static String CONSOLAS[] = {"PS4","XBOX ONE","WII U"};
+    public static int estrenoPS4XBX[] = {60, 3};
+    public static int normalPS4XBX[] = {50,5};
+    public static int estrenoWII[] = {50,4};
+    public static int normalWII[] = {40,6};
+    
+}
